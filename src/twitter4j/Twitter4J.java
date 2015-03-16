6f904c9dc5b7322ff98e2d02dfa0fb0e76be69db
aquinoacordero <aquinoacordero@eniac33.danielcastelao.org>
2015-03-16 08:58:44 +0100

    commit1examen

---
+++ src/twitter4j/Twitter4J.java
@@ -0,0 +1,18 @@
+package twitter4j;
+
+/**
+ *
+ * @author aquinoacordero
+ */
+
+public class Twitter4J {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
