4d737cab554e49e0bcbe8595a5a0cf4d44350830
aquinoacordero <aquinoacordero@eniac33.danielcastelao.org>
2015-03-16 09:42:02 +0100

    commit2examen-git

--- src/twitter4j/Twitter4J.java
+++ src/twitter4j/Twitter4J.java
@@ -2,19 +2,28 @@ package twitter4j;
 
 import java.util.logging.Level;
 import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.*;
+import static twitter4j.BuscarPorHash.BuscarPorHash;
+import static twitter4j.GetTimeLine.GetTimeLine;
+import static twitter4j.PostTwitter.PostTwitter;
 
 /**
  *
  * @author aquinoacordero
+ * @version 1.0.0
+ * @since 
+ * 
  */
 
 public class Twitter4J {
 
     /**
+     * Metodo principal que llama al los metodos postear, timeline y buscartuit
      * @param args the command line arguments
+     * 
      */
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         //Logger.getLogger(TwitterSimulacion.class.getName()).log(Level.SEVERE, null, ex);
         
         ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -22,6 +31,9 @@ public class Twitter4J {
         PostTwitter tuitear=new PostTwitter();
         GetTimeLine timeline=new GetTimeLine();
         BuscarPorHash buscartuit=new BuscarPorHash();
+        PostTwitter(twitter);
+        GetTimeLine(twitter);
+        BuscarPorHash(twitter);
     }
     }
     
